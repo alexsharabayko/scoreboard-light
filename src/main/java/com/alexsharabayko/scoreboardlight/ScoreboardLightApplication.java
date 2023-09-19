0d60425af385df1a2812c7758672e02f3dc74c50
alexander.sharabayko <alexander.sharabayko@glovoapp.com>
2023-09-19 21:01:55 +0200

    [TASK-0] feat: init tiny spring boot app

---
+++ src/main/java/com/alexsharabayko/scoreboardlight/ScoreboardLightApplication.java
@@ -0,0 +1,13 @@
+package com.alexsharabayko.scoreboardlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreboardLightApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ScoreboardLightApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/alexsharabayko/scoreboardlight/ScoreboardLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexsharabayko.scoreboardlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreboardLightApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
